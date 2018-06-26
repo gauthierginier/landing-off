960c4754a501221581ca4cf85070d6f535e10e3c
KarlMacabre <gwadalitokarl@gmail.com>
2018-06-25 22:48:52 -0400

    AjoutCard

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import Signing from '../../Images/signing.png';
+import Money from '../../Images/moneyBag.png';
+import Time from '../../Images/time.png';
+import Marker from '../../Images/MarkerMap.png';
+import Contrat from '../../Images/contract.png';
+
 
 class Card extends Component {
     render() {
         return (
-          <div class="card">
-          <div class="card-header">
-            <h2>{this.props.titre}</h2>
-          </div>
-          <div class="card-body">
-            <h3 className="card-typeContrat" >{this.props.type}</h3>
-            <h4>
-            <i class="fas fa-stopwatch"></i>ASAP 
 
-            <i class="fas fa-dollar-sign"></i>{this.props.remuneration}
-           
-            <i class="fas fa-map-marker-alt"></i>{this.props.lieu}
-           
-           <i class="fas fa-building"></i>{this.props.entreprise}
-            </h4>
-            
+         
+  <div class="card">
+  <div class="card-image waves-effect waves-block waves-light">
+    
+  </div>
+  <div class="card-content">
+    <span class="card-title grey-text text-darken-4">Card Title</span>
 
-          </div>
-          <a href="#" class="btn btn-primary">POSTULER</a>
-        </div>
+    <div class="divIcon">
+   <div class="divImgCard"><img src={Money} alt="money"/><p>35$ /heure</p></div>
+   <div class="divImgCard"><img src={Time} alt="time"/><p>35heures /semaine</p></div>
+    <div class="divImgCard"><img src={Marker} alt="marker"/>Jarry 97115<p></p></div>
+    <div class="divImgCard"><img src={Contrat} alt="contrat"/>CDD<p></p></div>
+    </div>
+   
+
+    <p><a href="#"><span class="card-title activator ">Card Title</span></a></p>
+  </div>
+  <div class="card-reveal">
+    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+  </div>
+</div>
     
          );
     }
