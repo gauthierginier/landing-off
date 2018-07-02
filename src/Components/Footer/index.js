cb817cac55bb23c118a9c0223cc9f91678fee2e9
KarlMacabre <gwadalitokarl@gmail.com>
2018-07-02 18:55:54 -0400

    MAJ

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <h1 class="h1Footer">Le site d'offium est en cours de développement ...</h1>
+        <h1 class="h1Footer">Le site d'officium est en cours de développement ...</h1>
         <h2>Made with <img src={Like} alt="Like" class="Like"/> by <img src={Konektey} alt="Konektey" class="Konektey"/></h2>
       </div>
     )
