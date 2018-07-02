c9aa2bbc1b3c41a8fa47432996c9ab09bc86ee7a
GauthierGinier <gauthier.ginier@gmail.com>
2018-07-02 16:04:33 -0400

    footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -8,19 +8,11 @@ export default class Footer extends Component {
          <footer class="page-footer">
           <div class="container">
             <div class="row">
-              <div class="col l6 s12">
+              <div class="col l12 s12">
                 <h5 class="white-text">Officium conseil</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <p class="grey-text text-lighten-4">Le site d'Officium-Conseil est en cours de développement...</p>
               </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Nos reseaux sociaux</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
+              
             </div>
           </div>
           <div class="footer-copyright">
